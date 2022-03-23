d8c5c13ab6f8d1e8f377e2fb7dfbf729acde4872
Tim N <nguyentimwork@gmail.com>
2022-03-23 16:52:48 +1100

    Incorporation of the Blueprint Function Library

---
+++ EditorWidgetFunctionLibrary/Source/EditorWidgetFunctionLibrary/Public/BPFL_EditorWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "BPFL_EditorWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EDITORWIDGETFUNCTIONLIBRARY_API UBPFL_EditorWidget : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
