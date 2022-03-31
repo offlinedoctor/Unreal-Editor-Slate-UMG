f3ad6d503d104d50ab12789419093884c98dbdd3
Tim N <nguyentimwork@gmail.com>
2022-04-01 02:35:45 +1100

    Implemented a new widget that lets you search for a folder path and prints out the folder path as an FString.

--- EditorWidgetFunctionLibrary/Source/EditorWidgetFunctionLibrary/EditorWidgetFunctionLibrary.Build.cs
+++ EditorWidgetFunctionLibrary/Source/EditorWidgetFunctionLibrary/EditorWidgetFunctionLibrary.Build.cs
@@ -48,6 +48,8 @@ public class EditorWidgetFunctionLibrary : ModuleRules
 				"ComponentVisualizers",
 				"TimeManagement",
 				"Kismet",
+				"Synthesis",
+				"DesktopWidgets",
 				// ... add private dependencies that you statically link with here ...	
 			}
 			);
---
+++ EditorWidgetFunctionLibrary/Source/EditorWidgetFunctionLibrary/Public/EW_Widget_FolderPicker.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/Widget.h"
+#include "Widgets/Input/SDirectoryPicker.h"
+#include "EW_Widget_FolderPicker.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EDITORWIDGETFUNCTIONLIBRARY_API UEW_Widget_FolderPicker : public UWidget
+{
+	GENERATED_BODY()
+
+	public:
+	virtual void ReleaseSlateResources(bool bRelease) override;
+
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	FString GetFilePath();
+
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	FString GetDirectory();
+
+	protected:
+	virtual TSharedRef<SWidget> RebuildWidget() override;
+	TSharedPtr<SDirectoryPicker> FolderPickerWidget;
+	
+};
